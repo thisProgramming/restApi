f7652f216b47451f252fd78572ce97e26de89f1d
thisProgramming <sindiguza@yahoo.com>
2019-03-04 00:28:08 +0100

    mongo Connection

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
 const port = 3000 || process.env.PORT;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+mongoose.connect('mongodb+srv://thisProgramming:123Sindiguza@restapi-zfkhy.gcp.mongodb.net/test?retryWrites=true/restApi', { useNewUrlParser: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection Error: '));
+
 
 app.listen(port, () => console.log(`Server Running at port ${port}...`));
